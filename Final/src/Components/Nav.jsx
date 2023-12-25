771bda3953aa2f47cc6654e5f24d49b6559c6e8b
tasalluhimi <tasneem.luhimi@gmail.com>
2023-12-25 17:49:51 +0300

    adding pages

---
+++ Final/src/Components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Nav
--- Final/src/Routes/Route.jsx
+++ Final/src/Routes/Route.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Routes, Route as R } from 'react-router-dom'
 import Home from '../Pages/Home'
 import Error from '../Errors/Error'
+import SignIn from '../Pages/SignIn'
+import SignUp from '../Pages/SignUp'
 
 function Route() {
   return (
@@ -9,6 +11,8 @@ function Route() {
         <Routes>
             <R path='/' element={<Home/>}></R>
             <R path='/*' element={<Error/>}></R>
+            <R path='/SignIn' element={<SignIn/>}></R>
+            <R path='/SignUp' element={<SignUp/>}></R>
         </Routes>
     </>
   )
