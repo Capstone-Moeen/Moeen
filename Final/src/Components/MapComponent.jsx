1cedb09ded534f2d034afbf892c93b54c99485ec
MaanRaddadi <maan.raddadi@gmail.com>
2023-12-26 10:11:10 +0300

    -  Added PlaceInfoSideCard (InCompelete)

--- Final/src/Components/MapComponent.jsx
+++ Final/src/Components/MapComponent.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { CircularProgress } from "@nextui-org/react";
 import FilterBtns from "./FilterBtns";
+import PlaceInfoSideCard from "./PlaceInfoSideCard";
+
 function MapComponent() {
   // Initializing the google maps with the api key
   const { isLoaded } = useLoadScript({
@@ -9,7 +11,7 @@ function MapComponent() {
   });
   const [userPosition, setUserPosition] = React.useState({});
   const [mapRef, setMapRef] = React.useState();
-
+   const [isOpen, setIsOpen] = useState(false)
   // Getting the user current location
   useEffect(() => {
     if (navigator.geolocation) {
@@ -25,19 +27,19 @@ function MapComponent() {
   //Storing the map reference in state to access it later
   const onMapLoad = (map) => {
     setMapRef(map);
-    map.panTo(userPosition);
   };
   // Handling the marker click event
   const handelMarkerClicked = (pos) => {
     mapRef.panTo(pos);
     mapRef.setZoom(16);
+    setIsOpen(true)
   };
 
   return (
     <>
       <div className=" h-[100vh] ">
         {!isLoaded ? (
-          <CircularProgress color="success" label="Loading..." />
+        <div className="w-full h-full flex justify-center items-center"> <CircularProgress size="lg" color="success" label="Loading..." /></div>  
         ) : (
           <GoogleMap
             zoom={13}
@@ -54,6 +56,7 @@ function MapComponent() {
               mapId: "a128fd791f572fa9",
             }}
             onLoad={onMapLoad}
+            onClick={() => setIsOpen(false)}
           >
             <FilterBtns></FilterBtns>
             <Marker
@@ -78,6 +81,7 @@ function MapComponent() {
               position={{ lat: 24.7, lng: 46.64 }}
               icon={"https://i.postimg.cc/VLQmsF0X/shopPin.png"}
             />
+        <PlaceInfoSideCard isOpen={isOpen}></PlaceInfoSideCard>
           </GoogleMap>
         )}
       </div>
--- Final/src/Components/Nav.jsx
+++ Final/src/Components/Nav.jsx
@@ -1,35 +1,30 @@
 import React from "react";
-import {
-
-  Input,
- Button
-} from "@nextui-org/react";
+import { Input, Button } from "@nextui-org/react";
 import { SearchIcon } from "./SearchIcon";
 function Nav() {
   return (
     <>
       <nav className="bg-[#005B41] h-[13vh] flex justify-between py-4 px-8 items-center">
-      <h1 className="text-white font-bold text-4xl">Logo</h1>
-      <div className="w-[28rem]">
-        <Input
-          classNames={{
-            base: " sm:max-w-[10rem] h-14",
-            mainWrapper: "h-full",
-            input: "text-large",
-            inputWrapper: "h-full font-normal text-default-500 bg-white w-[28rem]",
-            
-          }}
-          
-          placeholder="بحث"
-          size="lg"
-          endContent={<SearchIcon size={22} />}
-          type="search"
-          fullWidth={true}
-        />
+        <h1 className="text-white font-bold text-4xl">Logo</h1>
+        <div className="w-[35rem] max-sm:w-[16rem]">
+          <Input
+            classNames={{
+              base: " sm:max-w-[10rem] h-14 max-sm:hidden",
+              mainWrapper: "h-full",
+              input: "text-large",
+              inputWrapper:
+                "h-full font-normal text-default-500 bg-white w-[35rem] mr-10 max-sm:w-[4rem]",
+            }}
+            placeholder="بحث"
+            size="lg"
+            endContent={<SearchIcon size={22} />}
+            type="search"
+            fullWidth={true}
+          />
         </div>
-  <Button size="lg" className="text-black font-bold bg-white text-xl">
-      تسجيل جديد
-    </Button>
+        <Button size="lg" className="text-black font-bold bg-white text-xl max-sm:text-sm ">
+          تسجيل دخول
+        </Button>
       </nav>
     </>
   );
