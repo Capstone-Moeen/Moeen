21701f64f72d2344ef1452ad51e7e9633770d8d8
Amwaj-M <amwaj.mh3@gmail.com>
2023-12-26 21:51:58 +0300

    Edit

--- Final/src/Assets/Icons/SendIcon.jsx
+++ Final/src/Assets/Icons/SendIcon.jsx
@@ -7,5 +7,5 @@ export const SendIcon = ({
   height,
   ...props
 }) => (
-<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" ><path  d="M120-160v-240l320-80-320-80v-240l760 320-760 320Z" fill="white"/></svg>
+<svg  xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" style={{transform: ' scale(-1,1)'}} ><path  d="M120-160v-240l320-80-320-80v-240l760 320-760 320Z" fill="white" /></svg>
 );
