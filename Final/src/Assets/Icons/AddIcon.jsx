55efeea87cc2d6f27683bcb921f58e26155e1025
Amwaj-M <amwaj.mh3@gmail.com>
2023-12-26 21:40:36 +0300

    Request page+

---
+++ Final/src/Assets/Icons/AddIcon.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export const AddIcon = ({
+  icon,
+  size = 24,
+  strokeWidth = 1.5,
+  width,
+  height,
+  ...props
+}) => (
+<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" fill="white"/></svg>
+);
