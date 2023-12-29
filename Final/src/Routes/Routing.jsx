949b22d5e301551be058b87658c751cced20dfd8
tasalluhimi <tasneem.luhimi@gmail.com>
2023-12-29 18:42:35 +0300

    small edits c:

--- Final/src/Routes/Routing.jsx
+++ Final/src/Routes/Routing.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { Routes, Route as R } from 'react-router-dom'
 import Home from '../Pages/Home'
 import Error from '../Errors/Error'
-import SignIn from '../Pages/SignIn'
-import SignUp from '../Pages/SignUp'
 import NewRequest from '../Pages/NewRequest'
 import EasyLayout from '../Components/EasyLayout'
 
@@ -13,8 +11,6 @@ function Route() {
         <Routes>
             <R path='/' element={<Home/>}></R>
             <R path='/*' element={<Error/>}></R>
-            <R path='/SignIn' element={<SignIn/>}></R>
-            <R path='/SignUp' element={<SignUp/>}></R>
             <R path='/NewRequest' element={<NewRequest/>}></R>
             <R path='/EasyLayout' element={<EasyLayout/>}></R>
 
